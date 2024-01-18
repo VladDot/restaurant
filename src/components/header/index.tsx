bb57146742f878e4db7adafef77966ef2ce7d709
VladRomb <vladkostirya4@gmail.com>
2024-01-18 14:41:33 +0200

    test branch

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,7 @@
 export const Header = () => {
-    return <h1 className="text-red-600 ">HEADER</h1>;
+    return (
+        <header className="w-full p-3 bg-blue-600 border-2 border-blue-700">
+            <h1 className="text-red-600 p-2">HEADER</h1>;
+        </header>
+    );
 };
