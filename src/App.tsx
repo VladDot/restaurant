e8292a3aa028bb7487fce1218bc127ada7b3824f
JoeYlMS <ivbazulyk@gmail.com>
2024-01-18 12:21:29 +0200

    test-push

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="border-red-700 border-2 rounded-full">
-            <h1 className="text-red-500 text-4xl text-center">HEllO</h1>
+            <h1 className="text-red-500 text-4xl text-center">HEllO jfng;dfg</h1>
         </div>
     );
 }
