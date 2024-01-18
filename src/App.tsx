9a9ca2e207199e2d1aadb4d5df61a76f2caa0436
VladRomb <vladkostirya4@gmail.com>
2024-01-18 12:15:05 +0200

    add library : react-router-dom, formik, yup,clsx, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="border-red-700 border-2 rounded-full">
+            <h1 className="text-red-500 text-4xl text-center">HEllO</h1>
+        </div>
+    );
 }
 
 export default App;
