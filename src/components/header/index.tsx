80ece5c35f3909d94e6743b8c0d06dd6083cb0e9
JoeYlMS <ivbazulyk@gmail.com>
2024-01-19 13:07:37 +0200

    add green div

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,8 @@ export const Header = () => {
   return (
     <header className='w-full p-3 bg-blue-600 border-2 border-blue-700'>
       <h1 className='text-red-600 p-2'>HEADER igor</h1>
+
+      <div className='w-full h-[50px] bg-lime-500'></div>
     </header>
   );
 };
