9dcb7dbdd29e753520d6194096bac478651c6a5c
VladDot <107644242+VladDot@users.noreply.github.com>
2024-01-22 17:52:41 +0200

    add page url in constants (#7)
    create config in header
    start create header navbar

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,12 +1,43 @@
 export const routes = {
     home: "/",
-    menu:"/menu",
-    // menuCategory:{},
-    gallery: "/gallery",
+    bar: "/bar",
+    blog: "/blog",
+    menu: "/menu",
     about: "/about",
     admin: "/admin",
     login: "/login",
+    gallery: "/gallery",
+    banquet: "/banquet:category",
+    contacts: "/contacts",
+    menuBanquet: "/menu-banquet",
     registration: "/registration",
     privateOffice: "/privateOffice",
-    banquet: "/banquet",
-}
+
+    banquetCategory: {
+        banquetMenu: "/banquet/banquet-menu",
+    },
+
+    menuCategory: {
+        wok: "/menu/wok",
+        pasta: "/menu/pasta",
+        soups: "/menu/soups",
+        salads: "/menu/salads",
+        desserts: "/menu/desserts",
+        pastries: "/menu/pastries",
+        grillMenu: "/menu/grill-menu",
+        meatDishes: "/menu/meat-dishes",
+        fishDishes: "/menu/fish-dishes",
+        sideDishes: "/menu/side-dishes",
+        banquetMenu: "/menu/banquet-menu",
+        childrenMenu: "/menu/children-menu",
+        hotAppetizers: "/menu/hot-appetizers",
+        coldAppetizers: "/menu/cold-appetizers",
+        rollsAndSpringRolls: "/menu/rolls-and-spring-rolls",
+    },
+
+    galleryCategory: {
+        restaurantMenu: "/gallery/restaurant-menu",
+        interiorOfTheRestaurant: "/gallery/interior-of-the-restaurant",
+        sophisticationInEveryDetail: "/gallery/sophistication-in-every-detail",
+    },
+};
