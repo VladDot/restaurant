37b4b68f8378befb5e067166906dfb4b32e49b9c
VladRomb <vladkostirya4@gmail.com>
2024-01-31 15:23:00 +0200

    fix animation for dropdown

--- src/components/header/navBar/navBarDropMenu/index.tsx
+++ src/components/header/navBar/navBarDropMenu/index.tsx
@@ -1,28 +1,28 @@
 import { useState } from "react";
-import { getStyles } from "../../style";
 import { Link } from "react-router-dom";
+import { getStyles } from "../../style";
 import { NavBarLink } from "../../config";
 
 interface IMenuProps {
-    name: string;
-    categories?: NavBarLink[];
     idx: number;
     url: string;
+    name: string;
     title: string;
+    categories?: NavBarLink[];
 }
 
-export const NavBarDropMenu = ({
-    name,
-    categories,
-    idx,
-    url,
-    title,
-}: IMenuProps) => {
-    const [isArrow, setIsArrow] = useState(false);
+export const NavBarDropMenu = ({ url, title, categories }: IMenuProps) => {
+    const [isActive, setIsActive] = useState(false);
 
-    const { arrow, drop } = getStyles(isArrow);
-    console.log(isArrow);
+    const isBigMenu = !!categories && categories.length > 4;
 
+    const heightLi = isBigMenu ? 24 : 40;
+
+    const heightUl = isActive
+        ? `${categories && categories?.length * heightLi + 40}px`
+        : "0";
+
+    const { arrow, drop, subCategoryLink } = getStyles(isActive, isBigMenu);
     return (
         <div className="w-fit text-center h-full">
             {!categories && (
@@ -36,11 +36,11 @@ export const NavBarDropMenu = ({
 
             {categories && (
                 <div
-                    className="w-fit group text-center  hover:text-secondTextHover  duration-150 h-full"
+                    className="w-fit group text-center hover:text-secondTextHover  duration-150 h-full"
                     onMouseEnter={() => {
-                        setIsArrow(!isArrow);
+                        setIsActive(true);
                     }}
-                    onMouseLeave={() => setIsArrow(!isArrow)}
+                    onMouseLeave={() => setIsActive(false)}
                 >
                     <div className=" flex  justify-center items-center text-center h-full gap-4  ">
                         <Link
@@ -51,12 +51,15 @@ export const NavBarDropMenu = ({
                             <span className={arrow}></span>
                         </Link>
                     </div>
-                    {isArrow === true && (
-                        <ul className={drop}>
+                    {isActive && (
+                        <ul
+                            className={drop}
+                            style={{ height: heightUl }}
+                        >
                             {categories?.map(({ name, url }, idx) => (
                                 <Link
                                     to={url}
-                                    className=" p-2 pr-5 flex flex-col gap-5 text-black hover:text-secondTextHover text-[18px] hover:scale-125 transition-all duration-300 easy-in-out"
+                                    className={subCategoryLink}
                                     key={`drop_down_${name}_${idx}`}
                                 >
                                     {name}
