e3729cb9ca2bbf78d7211ea6054484e4e544f968
JoeYlMS <ivbazulyk@gmail.com>
2024-02-08 10:46:37 +0200

    add main banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -8,19 +8,19 @@ interface IBannerProps {
 }
 export const Banner = ({ src, title, content, className }: IBannerProps) => {
   return (
-    <section
+    <div
       style={{ backgroundImage: `url(${src})` }}
       className={clsx(
-        `w-full h-[65vh] mobile: bg-cover bg-[center] md:aspect-[2.42] relative flex items-center justify-center`,
+        `w-full h-full aspect-[1.11] mobile:bg-cover bg-[center] md:aspect-[2.42] relative flex items-center justify-center`,
         className
       )}
     >
       <div className='w-full h-full absolute z-0 bg-black opacity-30 top-0'></div>
 
-      <div className='container relative z-10 mx-auto p-5p flex flex-col gap-[20px] md:gap-[45px] text-white translate-y-[80px]'>
+      <div className='container relative z-10 mx-auto p-5p flex flex-col gap-[20px] md:gap-[35px] text-white translate-y-[80px]'>
         <p className='text-xl text-bannerContent text-[16px] md:text-2xl'>{content}</p>
         <h2 className='text-4xl md:text-7xl'>{title}</h2>
       </div>
-    </section>
+    </div>
   );
 };
