f4632a4b861441da983215003692021ddf6c7e54
VladRomb <vladkostirya4@gmail.com>
2024-02-09 17:09:42 +0200

    fixed some button and food-gallery

--- src/components/food-gallery/index.tsx
+++ src/components/food-gallery/index.tsx
@@ -1,32 +1,37 @@
-import { Button } from '../button';
-import { tmpSrcFood } from '../../mock';
-import { routes } from '../../constants/routes';
-import { ImgInterior } from '../../components';
+import { Button } from "../button";
+import { tmpSrcFood } from "../../mock";
+import { routes } from "../../constants/routes";
+import { ImgInterior } from "../../components";
 
-import './style.css';
+import "./style.css";
 
 export const FoodGallery = () => {
-  return (
-    <section className='container pt-20 items-center'>
-      <h2 className='mb-5 text-[18px] text-center font-serif text-secondText'>ГАЛЕРЕЯ СТРАВ</h2>
-      <h1 className='mb-20 text-[42px] text-center'>Вишуканість та натуральність</h1>
-      <div>
-        <ul className='flex gallery-dishes justify-between flex-wrap w-full gap-y-3'>
-          {tmpSrcFood.map(({ imgUrl, aspectMenu }, idx) => (
-            <ImgInterior
-              url={imgUrl}
-              aspect={aspectMenu}
-              className='gallery-dishes'
-              key={`ImageInterior-foodGallery-${idx}`}
+    return (
+        <section className="container pt-20 items-center">
+            <h2 className="mb-5 text-[18px] text-center font-serif text-secondText">
+                ГАЛЕРЕЯ СТРАВ
+            </h2>
+
+            <h1 className="mb-20 text-[42px] text-center">
+                Вишуканість та натуральність
+            </h1>
+
+            <ul className="flex gallery-dishes justify-between flex-wrap w-full gap-1 lg:gap-y-3 mb-4 lg:mb-8">
+                {tmpSrcFood.map(({ imgUrl, aspectMenu }, idx) => (
+                    <ImgInterior
+                        url={imgUrl}
+                        aspect={aspectMenu}
+                        className="gallery-dishes"
+                        key={`ImageInterior-foodGallery-${idx}`}
+                    />
+                ))}
+            </ul>
+
+            <Button
+                route={routes.gallery}
+                textContent="Дивитись усю галерею"
+                className="max-w-[250px] mx-auto"
             />
-          ))}
-        </ul>
-      </div>
-      <Button
-        route={routes.gallery}
-        textContent='Дивитись усю галерею'
-        className='w-[22%] m-auto mt-10 h-[60px]'
-      />
-    </section>
-  );
+        </section>
+    );
 };
