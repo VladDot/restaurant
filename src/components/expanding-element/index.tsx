1d0a3c30c40b9cc42f89041a4544091265b6f1f6
VladDot <107644242+VladDot@users.noreply.github.com>
2024-02-15 17:22:09 +0200

    Expanding element (#22)
    * create section ExpandingElement
    
    * finished visibleElement create hook useVisibleElement

---
+++ src/components/expanding-element/index.tsx
@@ -0,0 +1,47 @@
+import { FC, useRef, useState } from "react";
+import clsx from "clsx";
+
+import { useVisibleElement } from "../../hook";
+
+export interface IExpandingElement {
+    text: string;
+    Svg: React.FC;
+    className?: string;
+    classNameText?: string;
+}
+
+export const ExpandingElement: FC<IExpandingElement> = ({
+    Svg,
+    text,
+    className,
+    classNameText,
+}) => {
+    const elementRef = useRef<HTMLDivElement>(null);
+
+    const { isVisible } = useVisibleElement({ elementRef });
+
+    return (
+        <div
+            ref={elementRef}
+            className={clsx(
+                `cursor-default  w-full lg:w-1/4 last:w-full odd:bg-bgA78963 last:!bg-thirdBg even:bg-thirdBg p-[30px] group hover:!bg-black ${className} transition-all duration-500 easy-in-out `,
+                {
+                    "scale-100 animate-fadeIn duration-700": isVisible,
+                    "scale-0 ": !isVisible,
+                }
+            )}
+        >
+            <div className=" flex flex-col justify-center items-center  ">
+                <div className="w-[60px] fill-white group-hover:fill-secondTextHover duration-300">
+                    <Svg />
+                </div>
+
+                <h2
+                    className={`text-[24px] text-center whitespace-pre-line leading-7  lg:max-w-[250px]   group-hover:text-secondTextHover duration-300 easy-in-out text-white ${classNameText}`}
+                >
+                    {text}
+                </h2>
+            </div>
+        </div>
+    );
+};
--- src/pages/banquet/index.tsx
+++ src/pages/banquet/index.tsx
@@ -1,3 +1,19 @@
+import { IsvgBanquet, svgBanquet } from "./config";
+
+import { ExpandingElement } from "../../components";
+
 export const Banquet = () => {
-    return <>Banquet</>;
+    return (
+        <section className="w-full ">
+            <div className="lg:flex lg:flex-wrap w-full lg:[&>div:last-child>div>h2]:max-w-[350px]">
+                {svgBanquet.map(({ svg, text }: IsvgBanquet) => (
+                    <ExpandingElement
+                        Svg={svg}
+                        text={text}
+                        key={`ExpandingElement_${text}_1`}
+                    />
+                ))}
+            </div>
+        </section>
+    );
 };
