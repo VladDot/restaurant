f931d319bca8c44b7b0debc438e9a6caf4d40d88
JoeYlMS <ivbazulyk@gmail.com>
2024-02-19 15:43:57 +0200

    fix nav-burger

--- src/components/header/dropDownMenu/index.tsx
+++ src/components/header/dropDownMenu/index.tsx
@@ -1,89 +1,79 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-
-import { NavBarLink } from "../../../mock";
-import { getMenuStyles } from "../burger/style";
+import { NavBarLink } from '../../../mock';
+import { getMenuStyles } from '../burger/style';
 
 interface ListMenuProps {
-    url: string;
-    idx: number;
-    name: string;
-    title: string;
-    height?: string;
-    activeIdx: number;
-    categories?: NavBarLink[];
-    setActiveIdx: React.Dispatch<React.SetStateAction<number>>;
-    setIsActiveMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  url: string;
+  idx: number;
+  name: string;
+  title: string;
+  height?: string;
+  activeIdx: number;
+  categories?: NavBarLink[];
+  setActiveIdx: React.Dispatch<React.SetStateAction<number>>;
+  setIsActiveMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const DropDownMenu = ({
-    url,
-    idx,
-    title,
-    activeIdx,
-    categories,
-    setActiveIdx,
-    setIsActiveMenu,
+  url,
+  idx,
+  title,
+  activeIdx,
+  categories,
+  setActiveIdx,
+  setIsActiveMenu,
 }: ListMenuProps) => {
-    const isActive = idx === activeIdx;
+  const isActive = idx === activeIdx;
 
-    const { dropdown, btn, link } = getMenuStyles(isActive);
+  const { dropdown, btn, link } = getMenuStyles(isActive);
 
-    const heightUl = isActive
-        ? `${categories && categories?.length * 28}px`
-        : "0";
+  const heightUl = isActive ? `${categories && categories?.length * 28}px` : '0';
 
-    const handlerClick = () => {
-        if (isActive) {
-            setActiveIdx(-1);
-            return;
-        }
-        setActiveIdx(idx);
-    };
+  const handlerClick = () => {
+    if (isActive) {
+      setActiveIdx(-1);
+      return;
+    }
+    setActiveIdx(idx);
+  };
 
-    return (
-        <li className="list-none w-full flex flex-col gap-2">
-            <div className="flex w-full justify-between">
-                <Link
-                    to={url}
-                    className={link}
-                    onClick={() => setIsActiveMenu(false)}
-                >
-                    {title}
-                </Link>
-                {categories && (
-                    <span
-                        className={btn}
-                        onClick={() => handlerClick()}
-                    >
-                        {isActive ? "-" : "+"}
-                    </span>
-                )}
-            </div>
+  return (
+    <li className='list-none w-full flex flex-col gap-2'>
+      <div className='flex w-full justify-between'>
+        <Link to={url} className={link} onClick={() => setIsActiveMenu(false)}>
+          {title}
+        </Link>
+        {categories && (
+          <div
+            className='w-full flex justify-end cursor-pointer hover:bg-gray-200 rounded-full'
+            onClick={() => handlerClick()}
+          >
+            <span className={btn}>{isActive ? '-' : '+'}</span>
+          </div>
+        )}
+      </div>
 
-            {categories && (
-                <ul
-                    className={dropdown}
-                    style={{
-                        height: heightUl,
-                    }}
-                >
-                    {categories?.map((category, idx) => (
-                        <li
-                            className="w-full"
-                            key={`drop_down_${category.name}_${idx}`}
-                        >
-                            <Link
-                                to={category.url}
-                                onClick={() => setIsActiveMenu(false)}
-                                className="w-full block hover:text-secondTextHover  duration-150"
-                            >
-                                {category.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </li>
-    );
+      {categories && (
+        <ul
+          className={dropdown}
+          style={{
+            height: heightUl,
+          }}
+        >
+          {categories?.map((category, idx) => (
+            <li className='w-full' key={`drop_down_${category.name}_${idx}`}>
+              <Link
+                to={category.url}
+                onClick={() => setIsActiveMenu(false)}
+                className='w-full block hover:text-secondTextHover  duration-150'
+              >
+                {category.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  );
 };
