99a99d8acfa5653abee9e1fe873b956994f676f8
JoeYlMS <ivbazulyk@gmail.com>
2024-03-04 16:20:22 +0200

    add fonts raleway, Libre Baskerville, Source Serif 4; add in tailwind_config

--- src/components/main-banner/index.tsx
+++ src/components/main-banner/index.tsx
@@ -1,59 +1,61 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { Autoplay, EffectFade } from "swiper/modules";
-import { clsx } from "clsx";
+import { Autoplay, EffectFade } from 'swiper/modules';
+import { clsx } from 'clsx';
 
-import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-import { tmpSrc } from "../../mock";
+import { tmpSrc } from '../../mock';
+import { SubTitle, Title } from '../../components';
 
 interface IMainBannerProps {
-    className?: string;
+  className?: string;
 }
 
 export const MainBanner = ({ className }: IMainBannerProps) => {
   return (
-      <div className='w-full '>
-        <Swiper
-          loop={true}
-          speed={6000}
-          effect={'fade'}
-          allowTouchMove={false}
-          autoplay={{
-            delay: 1500,
-            disableOnInteraction: false,
-          }}
-          modules={[Autoplay, EffectFade]}
-          className='mySwiper'
-        >
-          {tmpSrc.map(({ imgUrl, content, title }, index) => {
-            return (
-              <SwiperSlide key={`img-url-${index}`}>
-                <div
-                  style={{ backgroundImage: `url(${imgUrl})` }}
-                  className={clsx(
-                    `w-full mobile:bg-cover bg-[center] aspect-[0.9] md:aspect-[2.42] relative flex items-center justify-center`,
-                    className
-                  )}
-                >
-                  <div className='w-full absolute z-0 bg-black opacity-30 top-0'></div>
+    <div className='w-full h-[100vh]'>
+      <Swiper
+        loop={true}
+        speed={6000}
+        effect={'fade'}
+        allowTouchMove={false}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, EffectFade]}
+        className='mySwiper'
+      >
+        {tmpSrc.map(({ imgUrl, content, title }, index) => {
+          return (
+            <SwiperSlide key={`img-url-${index}`}>
+              <div
+                style={{ backgroundImage: `url(${imgUrl})` }}
+                className={clsx(
+                  `w-full mobile:bg-cover bg-[center] h-[100vh] relative flex items-center justify-center`,
+                  className
+                )}
+              >
+                <div className='w-full absolute z-0 bg-black opacity-30 top-0'></div>
 
-                                <div className="container relative z-10 mx-auto p-5p flex flex-col gap-[20px] md:gap-[45px] text-white translate-y-[80px]">
-                                    <p className="text-xl text-bannerContent text-[16px] md:text-2xl">
-                                        {content}
-                                    </p>
-                                    <h2 className="text-4xl md:text-7xl">
-                                        {title}, {index + 1}
-                                    </h2>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
-        </div>
-    );
+                <div className='container relative z-10 mx-auto p-5 flex flex-col gap-[20px] md:gap-[45px] text-white translate-y-[80px]'>
+                  <Title
+                    uppercase
+                    text={title}
+                    className='text-xl text-start text-bannerContent text-[16px] md:text-2xl'
+                  />
+
+                  <SubTitle text={content} className='text-start font-sourceSerif max-w-[600px]' />
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 };
