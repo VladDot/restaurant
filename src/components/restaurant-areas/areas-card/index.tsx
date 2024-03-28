5c98ccbf470fe3af65a1611b92dd15c97cd5934b
JoeYlMS <105243926+JoeYlMS@users.noreply.github.com>
2024-03-28 16:18:10 +0200

    add all font (#55)

--- src/components/restaurant-areas/areas-card/index.tsx
+++ src/components/restaurant-areas/areas-card/index.tsx
@@ -1,78 +1,69 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Modal } from "../../modal";
-import { Button } from "../../button";
-import { getStyles } from "../../style";
-import { scrollToTop } from "../../../helpers";
+import { Modal } from '../../modal';
+import { Button } from '../../button';
+import { getStyles } from '../../style';
+import { scrollToTop } from '../../../helpers';
 
 interface IAreasCardProps {
-    link?: string;
-    text?: string;
-    imgSrc: string;
-    title?: string;
-    technicalEquipment?: string;
+  link?: string;
+  text?: string;
+  imgSrc: string;
+  title?: string;
+  technicalEquipment?: string;
 }
 
 export const AreasCard: React.FC<IAreasCardProps> = ({
-    text,
-    link,
-    title,
-    imgSrc,
-    technicalEquipment,
+  text,
+  link,
+  title,
+  imgSrc,
+  technicalEquipment,
 }) => {
-    const [isActive, setIsActive] = useState(false);
-    const [isBluerAnimate, setIsBluerAnimate] = useState<boolean | undefined>(
-        undefined
-    );
-    const { bluerAnimateFoodMenu } = getStyles({ isBluerAnimate });
-    return (
-        <div
-            className="relative w-full lg:w-[50%] h-[500px] overflow-hidden group/item"
-            onMouseMove={() => setIsBluerAnimate(true)}
-            onMouseLeave={() => setIsBluerAnimate(false)}
-        >
-            <div
-                className="relative scale-[1.5] lg:scale-[1.3] flex bg-cover h-full transition-all duration-500  easy-in-out group-hover/item:-translate-x-0 bg-transparent -translate-x-[60px]"
-                style={{ backgroundImage: `url(${imgSrc})` }}
-            />
+  const [isActive, setIsActive] = useState(false);
+  const [isBluerAnimate, setIsBluerAnimate] = useState<boolean | undefined>(undefined);
+  const { bluerAnimateFoodMenu } = getStyles({ isBluerAnimate });
+  return (
+    <div
+      className='relative w-full lg:w-[50%] h-[500px] overflow-hidden group/item'
+      onMouseMove={() => setIsBluerAnimate(true)}
+      onMouseLeave={() => setIsBluerAnimate(false)}
+    >
+      <div
+        className='relative scale-[1.5] lg:scale-[1.3] flex bg-cover h-full transition-all duration-500  easy-in-out group-hover/item:-translate-x-0 bg-transparent -translate-x-[60px]'
+        style={{ backgroundImage: `url(${imgSrc})` }}
+      />
 
-            <div className={bluerAnimateFoodMenu} />
+      <div className={bluerAnimateFoodMenu} />
 
-            <div className="absolute w-full h-full flex flex-col gap-y-[100px] justify-end items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-2">
-                <div className="flex flex-col justify-center items-center gap-y-5 group-hover/item:mb-10 transition-all duration-500  easy-in-out">
-                    <h1 className="text-xl mb-3 text-center text-white w-[150px]">
-                        {title}
-                    </h1>
+      <div className='absolute w-full h-full flex flex-col gap-y-[100px] justify-end items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-2'>
+        <div className='flex flex-col justify-center items-center gap-y-5 group-hover/item:mb-10 transition-all duration-500  easy-in-out'>
+          <h1 className='text-xl mb-3 text-center text-white'>{title}</h1>
 
-                    <p className="text-white text-center text-lg">{text}</p>
+          <p className='text-white text-center text-lg'>{text}</p>
 
-                    <Button
-                        route={link}
-                        textContent="Відкрити меню"
-                        onClick={() => scrollToTop("auto")}
-                        className="max-w-[280px] [&>span]:text-white "
-                    />
-                </div>
-
-                <Button
-                    type="button"
-                    textContent="Відкрити меню"
-                    onClick={() => setIsActive(true)}
-                    className="max-w-[250px]  [&>span]:text-white "
-                />
-                {isActive && (
-                    <Modal
-                        isOpen={isActive}
-                        onClose={setIsActive}
-                    >
-                        <div className="max-w-[280px] sm:max-w-[400px] w-full  bg-thirdBg relative top-1/2  -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center p-4 border-2 rounded-lg shadow-md shadow-white">
-                            <p className="text-center text-lg text-white">
-                                {technicalEquipment}
-                            </p>
-                        </div>
-                    </Modal>
-                )}
-            </div>
+          <Button
+            route={link}
+            textContent='Відкрити меню'
+            onClick={() => scrollToTop('auto')}
+            className='max-w-[280px] [&>span]:text-white '
+          />
         </div>
-    );
+
+        <Button
+          type='button'
+          textContent='Відкрити меню'
+          onClick={() => setIsActive(true)}
+          className='max-w-[250px]  [&>span]:text-white '
+        />
+        {isActive && (
+          <Modal isOpen={isActive} onClose={setIsActive}>
+            <div className='max-w-[280px] sm:max-w-[400px] w-full  bg-thirdBg relative top-1/2  -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center p-4 border-2 rounded-lg shadow-md shadow-white'>
+              <p className='text-center text-lg text-white'>{technicalEquipment}</p>
+            </div>
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
 };
--- src/pages/bar/index.tsx
+++ src/pages/bar/index.tsx
@@ -1,18 +1,12 @@
-import { Banner } from "../../components";
-import ImgBar from "../../assets/img/bar-1.jpg";
+import { Banner } from '../../components';
+import ImgBar from '../../assets/img/bar-1.jpg';
 export const Bar = () => {
-    return (
-        <>
-            <Banner
-                title="Bar"
-                content="Bar Bar Bar"
-            />
-            <div className="container py-10 flex justify-center">
-                <img
-                    src={ImgBar}
-                    alt=""
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Banner title='Bar' content='Bar Bar Bar' />
+      <div className='container py-10 flex justify-center'>
+        <img src={ImgBar} alt='img card bar' />
+      </div>
+    </>
+  );
 };
