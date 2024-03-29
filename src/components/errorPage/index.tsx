375ab96a141a594fe43d235f48ebc5c81ca3ba41
JoeYlMS <ivbazulyk@gmail.com>
2024-03-29 12:35:07 +0200

    fix error page

--- src/components/errorPage/index.tsx
+++ src/components/errorPage/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import ImgError from '../../assets/img/house.jpg';
 import { Button } from '../../components';
+import ImgError from '../../assets/img/house.jpg';
 interface IErrorsProps {
-  text: string;
   url: string;
+  text: string;
 }
 
 export const Errors: React.FC<IErrorsProps> = ({ text, url }) => {
   return (
     <section
-      className='h-screen bg-cover bg-center flex flex-col justify-center items-center'
       style={{ backgroundImage: `url(${ImgError})` }}
+      className='h-screen bg-cover bg-center flex flex-col justify-center items-center'
     >
       <div className='w-full h-full absolute z-0 bg-black opacity-30 top-0' />
 
@@ -19,7 +19,7 @@ export const Errors: React.FC<IErrorsProps> = ({ text, url }) => {
         className='container bg-green flex flex-col justify-center items-center gap-9 md:gap-20 
           overflow-y-hidden relative z-10'
       >
-        <h1 className='font-secondFont mobile:max-w-[80%] md:max-w-[70%] xl:max-w-[inherit] text-subTitleClamp text-center text-secondText'>
+        <h1 className='font-secondFont text-secondText text-subTitleClamp text-center select-none mobile:max-w-[80%] md:max-w-[70%] xl:max-w-[inherit] '>
           Oops! We can't find the page you're looking for
         </h1>
         <Button
